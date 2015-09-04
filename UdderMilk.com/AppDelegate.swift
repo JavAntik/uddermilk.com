78d6d3f6a916476b59710581453004f87868b6a6
Marina Markova <marina@marinas-macbook-pro.local>
2015-09-04 17:00:53 -0500

    added basic data structure

--- UdderMilk.com/AppDelegate.swift
+++ UdderMilk.com/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
@@ -50,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-        if topAsDetailController.detailItem == nil {
+        if topAsDetailController.category == nil {
             // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
             return true
         }
---
+++ UdderMilk.com/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  UdderMilk.com
+//
+//  Created by Marina Markova on 9/4/15.
+//  Copyright © 2015 Anton Baranchuk. All rights reserved.
+//
+
+import Foundation
+
+class Category {
+    let name: String
+    let description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
